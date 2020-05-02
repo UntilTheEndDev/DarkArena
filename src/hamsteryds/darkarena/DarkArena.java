6c5e1e414754386e46ffc92ad3c14b4fb6667ba4
南外丶仓鼠 <15717993503@163.com>
2020-05-02 21:52:26 +0800

    UPDATE

--- src/hamsteryds/darkarena/DarkArena.java
+++ src/hamsteryds/darkarena/DarkArena.java
@@ -1,9 +1,15 @@
 package hamsteryds.darkarena;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import hamsteryds.darkarena.warlord.PAPIExpansion;
 import hamsteryds.darkarena.warlord.WarlordManager;
@@ -28,6 +34,8 @@ public class DarkArena extends JavaPlugin {
 		new PAPIExpansion().register();
 		
 		ArchmageManager.initAll();
+		
+		checkUpdate();
 	}
 
 	@Override
@@ -83,4 +91,46 @@ public class DarkArena extends JavaPlugin {
 		}
 		return true;
 	}
+	String latestVersion;
+    boolean isLatest = true;
+	public void checkUpdate() {
+        new BukkitRunnable() {
+            public void run() {
+                latestVersion = getLatestVersion();
+                if (latestVersion == null) {
+                    return;
+                }
+                if (latestVersion.equalsIgnoreCase(getDescription().getVersion())) {
+                    getServer().getConsoleSender().sendMessage("战争领主-插件已经是最新版啦~");
+                } else {
+                	getServer().getConsoleSender().sendMessage("战争领主-插件不是最新稳定版，请更换~");
+                    isLatest = false;
+                }
+            }
+        }.runTaskAsynchronously(this);
+    }
+
+    public static String getLatestVersion() {
+        HttpURLConnection connection = null;
+        try {
+            int timeout = 5000;
+            URL url = new URL("https://untiltheend.coding.net/p/UntilTheEnd/d/UntilTheEnd/git/raw/master/UTEversion.txt");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(timeout);
+            InputStream inStream = connection.getInputStream();
+            final StringBuilder builder = new StringBuilder(255);
+            int byteRead;
+            while ((byteRead = inStream.read()) != -1) {
+                builder.append((char) byteRead);
+            }
+            return builder.toString();
+        } catch (Exception exception) {
+            DarkArena.instance.getLogger().log(Level.WARNING, "Failed to get update info.", exception);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return null; //这个return代表获取不到版本时返回的字符串，当然你可以改别的
+    }
 }
